792b8f40e54fc4b1d66cfb9cb7d83671e04cf5ee
Adwait Kulkarni <adwaitk@beatenearly-lm.corp.yahoo.com>
2014-07-15 19:24:01 -0700

    created model for petsitter

--- src/com/petsitterfinder/datamodel/PetSitter.java
+++ src/com/petsitterfinder/datamodel/PetSitter.java
@@ -1,5 +1,82 @@
 package com.petsitterfinder.datamodel;
 
-public class PetSitter {
+import com.parse.GetCallback;
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
+@ParseClassName("PetSitter")
+public class PetSitter extends ParseObject {
+
+	public PetSitter() {
+		super();
+	}
+	
+	//city
+	public void setAge(int age) {
+		put("age", age);
+	}
+	
+	public int getAge() {
+		return getInt("age");
+	}
+	
+	//houseCalls
+	public void setHouseCalls(Boolean houseCalls) {
+		put("houseCalls", houseCalls);
+	}
+	
+	public Boolean getHouseCalls() {
+		return getBoolean("houseCalls");
+	}
+	
+	//name
+	public void setName(String Name) {
+		put("name", Name);
+	}
+	
+	public String getName() {
+		return getString("name");
+	}
+	
+	//overNight
+	public void setOverNight(Boolean overNight) {
+		put("houseBroken", overNight);
+	}
+	
+	public Boolean getOverNight() {
+		return getBoolean("overNight");
+	}
+	
+	//profileImgUrl
+	public void setProfileImgUrl(String profileImgUrl) {
+		put("profileImgUrl", profileImgUrl);
+	}
+	
+	public String getProfileImgUrl() {
+		return getString("profileImgUrl");
+	}
+	
+	//rating
+	public void setRating(int rating) {
+		put("rating", rating);
+	}
+	
+	public int getRating() {
+		return getInt("rating");
+	}
+	
+	//zip
+	public void setZip(int zip) {
+		put("zip", zip);
+	}
+	
+	public int getZip() {
+		return getInt("zip");
+	}
+	
+	public static void getPetSitter(String id, GetCallback<PetSitter> callback) {
+		ParseQuery<PetSitter> query = ParseQuery.getQuery(PetSitter.class);
+		query.getInBackground(id, callback);	
+	}
 }
